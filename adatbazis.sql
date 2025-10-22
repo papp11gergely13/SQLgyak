b489e071dbb7180af927694c9a24c4646bd24e85
papp11gergely13 <papp11gergely13@gmail.com>
2025-10-22 09:40:53 +0200

    update
    added meal table

--- adatbazis.sql
+++ adatbazis.sql
@@ -1,3 +1,18 @@
 CREATE DATABASE adatbazis
 CHARACTER SET "utf8"
-COLLATE "utf8_hungarian_ci"
+COLLATE "utf8_hungarian_ci";
+
+USE adatbazis;
+
+CREATE TABLE meals(
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    mealsName VARCHAR(100) NOT NULL,
+    qantity INT NOT NULL,
+    price INT NOT NULL,
+    expirationDate DATETIME DEFAULT CURRENT_TIMESTAMP,
+    isSeasonal BOOLEAN DEFAULT FALSE,
+    placeOrigin VARCHAR(50),
+    mealDescription TEXT,
+    allergens JSON,
+    createdAt DATETIME DEFAULT CURRENT_TIMESTAMP,
+    updatedAT DATETIME DEFAULT CURRENT_TIMESTAMP);
